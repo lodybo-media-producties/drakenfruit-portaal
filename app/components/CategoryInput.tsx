90f2d3fcc24567c48b7c412a2f9dfe90a5d255e6
Lody Borgers <lodyborgers@gmail.com>
2023-10-02 10:08:19 +0200

    feature/dra 45 adding categories on the fly (#13)

--- app/components/CategoryInput.tsx
+++ app/components/CategoryInput.tsx
@@ -1,10 +1,18 @@
 import { type SerializeFrom } from '@remix-run/server-runtime';
-import { type Category } from '@prisma/client';
+import { type Category } from '~/models/categories.server';
 import { Combobox } from '~/components/ui/combobox';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import Message from '~/components/Message';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '~/components/ui/dialog';
+import CategoryMutationForm from '~/components/CategoryMutationForm';
 
 export type CategorySelection = Pick<SerializeFrom<Category>, 'id' | 'name'>;
 
@@ -21,8 +29,10 @@ export default function CategoryInput({
   error,
   onSelect,
 }: Props) {
-  const [selectedCategories, setSelectedCategories] =
-    useState(initialCategories);
+  const [selectedCategories, setSelectedCategories] = useState(
+    initialCategories ?? []
+  );
+  const [open, setOpen] = useState(false);
 
   const { t, i18n } = useTranslation('components');
   const i = i18n.language === 'nl' ? 'nl' : 'en';
@@ -31,8 +41,17 @@ export default function CategoryInput({
     value: category.id,
     label: category.name[i],
   }));
+  options.push({
+    value: '_unselectable_custom',
+    label: t('CategoryInput.Custom Category Option Label'),
+  });
 
   const handleCategorySelect = (selectedValues: string[]) => {
+    if (selectedValues.includes('_unselectable_custom')) {
+      setOpen(true);
+      return;
+    }
+
     setSelectedCategories(selectedValues);
     onSelect?.(selectedValues);
   };
@@ -42,34 +61,63 @@ export default function CategoryInput({
     .map((category) => category.name[i])
     .join(', ');
 
+  const handleCategoryCreate = (category: Category) => {
+    const newSelectedCategories = [...selectedCategories, category.id];
+    setSelectedCategories(newSelectedCategories);
+    onSelect?.(newSelectedCategories);
+
+    setOpen(false);
+  };
+
   return (
     <div className="flex flex-col gap-2">
-      <Combobox
-        multiple
-        initialValues={selectedCategories}
-        options={options}
-        placeholder={t('CategoryInput.Placeholder')}
-        triggerLabel={t('CategoryInput.Trigger Label')}
-        notFoundMessage={t('CategoryInput.Not Found Message')}
-        onSelect={handleCategorySelect}
-        error={error}
-        showSelectedInTrigger={false}
-      />
-      {selectedCategories?.length ? (
-        <div className="text-dark-blue">
-          {t('CategoryInput.Selected Categories Label', {
-            categories: selectedCategoriesList ?? '',
-            count: selectedCategories?.length,
-          })}
-        </div>
-      ) : null}
-      <Message variant="error" message={error} subtle />
-      <input
-        className="h-0"
-        type="hidden"
-        name="categoryIDs"
-        value={selectedCategories}
-      />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <Combobox
+          multiple
+          initialValues={selectedCategories}
+          options={options}
+          placeholder={t('CategoryInput.Placeholder')}
+          triggerLabel={t('CategoryInput.Trigger Label')}
+          notFoundMessage={t('CategoryInput.Not Found Message')}
+          onSelect={handleCategorySelect}
+          error={error}
+          showSelectedInTrigger={false}
+        />
+        {selectedCategories?.length ? (
+          <div className="text-dark-blue">
+            {t('CategoryInput.Selected Categories Label', {
+              categories: selectedCategoriesList ?? '',
+              count: selectedCategories?.length,
+            })}
+          </div>
+        ) : null}
+        <Message variant="error" message={error} subtle />
+        <input
+          className="h-0"
+          type="hidden"
+          name="categoryIDs"
+          value={selectedCategories}
+        />
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              {t('CategoryInput.Custom Category Dialog.Title')}
+            </DialogTitle>
+            <DialogDescription>
+              {t('CategoryInput.Custom Category Dialog.Description')}
+            </DialogDescription>
+          </DialogHeader>
+
+          <div>
+            <CategoryMutationForm
+              mode="create"
+              onCreatedOrEdited={handleCategoryCreate}
+              noSubmit
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
